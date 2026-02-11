3358a77d0c6f5ab7488df8876aa70e4d93ce95a8
Daniel Cybulka <danielcybulka@gmail.com>
2026-02-11 15:47:27 +0100

    initial repo split

--- contracts/eslint.config.mjs
+++ contracts/eslint.config.mjs
@@ -1,16 +1,35 @@
-import tseslint from 'typescript-eslint';
-import baseConfig from '../../eslint.config.mjs';
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  ...baseConfig,
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      parser: tseslint.parser,
       parserOptions: {
-        project: [new URL('./tsconfig.lib.json', import.meta.url).pathname],
-        tsconfigRootDir: new URL('.', import.meta.url).pathname,
-      },
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname
+      }
     },
+    rules: {
+    ...tseslint.configs.recommendedTypeChecked.rules,
+    // TODO: Clean warnings
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-misused-promises": "warn"
+  }
+  },
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: { "prettier/prettier": "error" },
   },
 ];
