aec2bf200b219ab1936362ec8f5313b252cbfb25
Daniel Cybulka <danielcybulka@gmail.com>
2026-02-13 14:39:30 +0100

    update naming in contracts script to be consistent

--- contracts/packages/contracts/build.sh
+++ contracts/packages/contracts/build.sh
@@ -3,16 +3,16 @@ set -e
 
 # Paths
 ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
-CONTRACTS_DIR="$ROOT_DIR/packages/contracts"
+PACKAGE_DIR="$ROOT_DIR/packages/contracts"
 
 SRC_ARTIFACTS_DIR="$ROOT_DIR/artifacts"
 SRC_CACHE_DIR="$ROOT_DIR/cache"
 
-DEST_ABI_DIR="$CONTRACTS_DIR/abis"
+DEST_ABI_DIR="$PACKAGE_DIR/abis"
 
 rm -rf "$SRC_ARTIFACTS_DIR" "$SRC_CACHE_DIR"
 
-find "$CONTRACTS_DIR" -mindepth 1 ! -name 'package.json' ! -name 'build.sh' -exec rm -rf {} +
+find "$PACKAGE_DIR" -mindepth 1 ! -name 'package.json' ! -name 'build.sh' -exec rm -rf {} +
 
 npx hardhat compile
 
@@ -28,5 +28,5 @@ find "$SRC_ARTIFACTS_DIR/contracts" -type f -name "*.json" \
 find "$SRC_ARTIFACTS_DIR/@openzeppelin/contracts" -type f -name "ERC1967Proxy.json" \
   -exec cp {} "$DEST_ABI_DIR" \;
 
-cp -r $ROOT_DIR/contracts/* "$CONTRACTS_DIR/"
+cp -r $ROOT_DIR/contracts/* "$PACKAGE_DIR/"
 
