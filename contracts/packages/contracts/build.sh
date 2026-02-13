654158e1dae506a9d353ea47b31692f68d428551
Daniel Cybulka <danielcybulka@gmail.com>
2026-02-13 13:29:28 +0100

    add abis to contracts

---
+++ contracts/packages/contracts/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# Paths
+ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+CONTRACTS_DIR="$ROOT_DIR/packages/contracts"
+
+SRC_ARTIFACTS_DIR="$ROOT_DIR/artifacts"
+SRC_CACHE_DIR="$ROOT_DIR/cache"
+
+DEST_ABI_DIR="$CONTRACTS_DIR/abis"
+
+rm -rf "$SRC_ARTIFACTS_DIR" "$SRC_CACHE_DIR"
+
+find "$CONTRACTS_DIR" -mindepth 1 ! -name 'package.json' ! -name 'build.sh' -exec rm -rf {} +
+
+npx hardhat compile
+
+mkdir -p "$DEST_ABI_DIR"
+
+# Find and copy JSON files (exclude dbg.json and testing folder)
+find "$SRC_ARTIFACTS_DIR/contracts" -type f -name "*.json" \
+  ! -name "*.dbg.json" \
+  ! -path "*/testing/*" \
+  -exec cp {} "$DEST_ABI_DIR" \;
+
+# Add proxy from @oz
+find "$SRC_ARTIFACTS_DIR/@openzeppelin/contracts" -type f -name "ERC1967Proxy.json" \
+  -exec cp {} "$DEST_ABI_DIR" \;
+
+cp -r $ROOT_DIR/contracts/* "$CONTRACTS_DIR/"
+
